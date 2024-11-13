ac7f1f020e48cd277852035f4f11e37b8dd22036
Romanenko Yliia(mac) <romany0507306677@gmail.com>
2024-11-13 12:29:00 +0200

    add delete commit

--- js/tesk-1.js
+++ js/tesk-1.js
@@ -1,9 +1,9 @@
 function slugify(title) {
- const slug = title.toLowerCase().trim().split(' ').join('-');
-return(slug);
+  const slug = title.toLowerCase().trim().split(" ").join("-");
+  return slug;
 }
 
 console.log(slugify("Arrays for beginners"));
 console.log(slugify("English for developer"));
 console.log(slugify("Ten secrets of JavaScript"));
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); 
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
