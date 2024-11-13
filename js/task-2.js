b79085b162e9769eadc9da9911f2831cc05c7061
Romanenko Yliia(mac) <romany0507306677@gmail.com>
2024-11-13 12:14:27 +0200

    add commit in task2 and task3

--- js/task-2.js
+++ js/task-2.js
@@ -4,16 +4,15 @@ const sumArray = firstArray.concat(secondArray);
 
 if (sumArray.length > maxLength) {
 const newCopyArray = sumArray.slice(0, maxLength);
-console.log(newCopyArray);
+return newCopyArray;
 } else {
   return sumArray;
 }
-
 }
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); 
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); 
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
+// console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
+// console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
+// console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); 
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); 
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
 
--- js/task-3.js
+++ js/task-3.js
@@ -7,9 +7,9 @@ for (let i = 0; i < numbers.length; i++) {
 }
 return newArray;
 }
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([1, 2, 3, 4, 5], 3)); 
-console.log(filterArray([1, 2, 3, 4, 5], 4)); 
-console.log(filterArray([1, 2, 3, 4, 5], 5)); 
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20)); 
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 3)); 
+// console.log(filterArray([1, 2, 3, 4, 5], 4)); 
+// console.log(filterArray([1, 2, 3, 4, 5], 5)); 
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20)); 
